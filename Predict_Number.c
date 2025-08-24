19c0409e64c8d088b27cfb4c806bc0e0ec189d51
RahulLo76 <rahulo7611111@gmail.com>
2025-08-24 12:29:42 +0530

    Add files via upload

---
+++ Predict_Number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+    srand(time(0));
+int predict_number;
+int random_number = (rand()%1000)+1;
+int no_of_pridiction = 0;
+
+printf("Enter the Numbers between 1 to 1000\n");
+do
+{
+  scanf("%d", &predict_number);
+  if(predict_number>random_number)
+  {
+    printf("Lower Number please\n");
+  }
+  else if(predict_number<random_number)
+  {
+    printf("Higher Number Please\n");
+  }
+  else{
+    printf("Congrats! You Predicited The Number!!\n");
+  }
+  no_of_pridiction++;
+}while(predict_number!=random_number);
+printf("You have taken %d attempts\n",no_of_pridiction);
+return 0;
+}
